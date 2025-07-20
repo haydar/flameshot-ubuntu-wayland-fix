00cada00e12c39d19c7d61bf0e2f2eea853d2dd3
Haydar ŞAHİN <haydarsahin34@gmail.com>
2025-07-20 04:36:07 +0300

    Refactor Flameshot installation script to dynamically find available custom keybinding slots

--- flameshot-install.sh
+++ flameshot-install.sh
@@ -25,22 +25,38 @@ sudo apt install -y flameshot
 SHORTCUT_NAME="flameshot"
 SHORTCUT_CMD='sh -c -- "QT_QPA_PLATFORM=wayland flameshot gui"'
 SHORTCUT_KEY="Print"
-BINDING_PATH="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
 
 echo "[+] Configuring GNOME custom shortcut..."
-CURRENT_BINDINGS=$(gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings)
 
-if [[ "$CURRENT_BINDINGS" != *"$BINDING_PATH"* ]]; then
-    if [[ "$CURRENT_BINDINGS" == "@as []" ]]; then
-        NEW_BINDINGS="['$BINDING_PATH']"
-    else
-        NEW_BINDINGS=$(echo "$CURRENT_BINDINGS" | sed "s/]$/, '$BINDING_PATH']/")
+# Check for existing custom keybindings and add a new one if necessary.
+# This part is crucial for not overwriting existing custom keybindings.
+# We find the next available custom binding slot.
+BINDING_PATH=""
+for i in $(seq 0 99); do # Check up to custom99
+    TEMP_BINDING_PATH="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom$i/"
+    if ! gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings | grep -q "$TEMP_BINDING_PATH"; then
+        BINDING_PATH="$TEMP_BINDING_PATH"
+        break
     fi
-    gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "$NEW_BINDINGS"
+done
+
+if [ -z "$BINDING_PATH" ]; then
+    echo "❌ Could not find an available slot for a new custom shortcut. Please check your existing shortcuts."
+    exit 1
 fi
 
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$BINDING_PATH name "$SHORTCUT_NAME"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$BINDING_PATH command "$SHORTCUT_CMD"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$BINDING_PATH binding "$SHORTCUT_KEY"
+CURRENT_BINDINGS=$(gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings)
+if [[ "$CURRENT_BINDINGS" == "@as []" ]]; then
+    NEW_BINDINGS="['$BINDING_PATH']"
+else
+    # Remove the trailing ']' and append the new path, then add ']' back
+    NEW_BINDINGS="${CURRENT_BINDINGS::-1}, '$BINDING_PATH']"
+fi
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "$NEW_BINDINGS"
+
+# Set the properties for the new custom keybinding
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:"$BINDING_PATH" name "$SHORTCUT_NAME"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:"$BINDING_PATH" command "$SHORTCUT_CMD"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:"$BINDING_PATH" binding "$SHORTCUT_KEY"
 
 echo "[✓] Done! Flameshot is now bound to the Print key under Wayland."
